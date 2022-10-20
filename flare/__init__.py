db962233eb7c8dceb3f59bc5eb641286f2a86c61
Lunarmagpie <bambolambo0@gmail.com>
2022-10-20 14:23:25 -0400

    re-export

--- flare/__init__.py
+++ flare/__init__.py
@@ -3,5 +3,6 @@ import typing
 from flare.handle_resp import install
 from flare.context import Context
 from flare.component import button
+from flare.converters import Converter, add_converter
 
-__all__: typing.Sequence[str] = ("install", "Context", "button")
+__all__: typing.Sequence[str] = ("install", "Context", "button", "Converter", "add_converter")
