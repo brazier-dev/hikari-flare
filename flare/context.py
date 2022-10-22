f1566cf691c30368285424e4e22edbf52fcdbf5a
Lunarmagpie <bambolambo0@gmail.com>
2022-10-22 18:01:02 -0400

    add get_components

--- flare/context.py
+++ flare/context.py
@@ -5,6 +5,7 @@ import typing as t
 
 import hikari
 from hikari.snowflakes import Snowflake
+from flare import row
 
 __all__: t.Sequence[str] = ("Context", "InteractionResponse")
 
@@ -232,6 +233,10 @@ class Context:
     def get_channel(self) -> t.Optional[hikari.TextableGuildChannel]:
         """Gets the channel this context represents, None if in a DM. Requires application cache."""
         return self._interaction.get_channel()
+
+    def get_components(self) -> t.MutableSequence[row.Row]:
+        """Returns the flare components for the interaction this context is proxying."""
+        return row.Row.from_message(self.message)
 
     async def get_last_response(self) -> InteractionResponse:
         """Get the last response issued to the interaction this context is proxying.
