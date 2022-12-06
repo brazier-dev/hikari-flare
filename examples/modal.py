0e51640a20eecf318bd10551a53b006d8d4832dc
Lunarmagpie <bambolambo0@gmail.com>
2022-12-06 16:05:54 -0500

    add commends

--- examples/modal.py
+++ examples/modal.py
@@ -16,7 +16,8 @@ class ModalTest(flare.Modal, title="My Title"):
     text_input_2: flare.TextInput = flare.TextInput(label="Test label 2")
 
     async def callback(self, ctx: flare.ModalContext) -> None:
-        # You can access text inputs with `self.text_input`
+        # You can access text inputs with `self.text_input`.
+        # These will be the first two cells in `ctx.values`.
         print(self.text_input.value)
         print(self.text_input_2.value)
 
@@ -27,7 +28,7 @@ class ModalTest(flare.Modal, title="My Title"):
 
 
 # Function that has `interaction` as an arg.This represents a result from your command handler.
-async def magic_function(interaction: hikari.ModalResponseMixin):
+async def magic_entrypoint(interaction: hikari.CommandInteraction | hikari.ComponentInteraction):
     modal = ModalTest(5)
     await modal.send(interaction)
 
